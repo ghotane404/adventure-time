bd27b8da0907ed4a2cb38cf6f9a2799b3dfa2075
ghotane404 <62863408+ghotane404@users.noreply.github.com>
2026-04-22 11:14:35 -0700

    add loadAdventure

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,11 +1,30 @@
 package com.pluralsight;
 import java.util.*;
+import java.io.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    static void main() throws IOException {
 
+        loadAventure();
 
+    }
 
+    public static void loadAventure() throws IOException {
+        FileReader fileReader = new FileReader("adventure1.csv");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String line = bufferedReader.readLine();
+
+        try {
+            while (line != null){
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
